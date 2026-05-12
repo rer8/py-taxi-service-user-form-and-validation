44d90314894f903785bda93e3a57d9f3a5b0331c
m3 <sergey.radik@gmail.com>
2026-05-12 16:10:29 +0300

    Done

--- taxi/urls.py
+++ taxi/urls.py
@@ -69,7 +69,7 @@ urlpatterns = [
     path("drivers/create/",
          DriverCreateView.as_view(),
          name="driver-create"),
-    path("drivers/<int:pk>/update/ ",
+    path("drivers/<int:pk>/update/",
          DriverUpdateView.as_view(),
          name="driver-update"),
     path("drivers/<int:pk>/delete/",
