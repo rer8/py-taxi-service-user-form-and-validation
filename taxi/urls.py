9fa94253bb818076ae320fc927662a85b89d898e
m3 <sergey.radik@gmail.com>
2026-05-12 12:56:07 +0300

    Done

--- taxi/urls.py
+++ taxi/urls.py
@@ -20,7 +20,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", index, name="index"),
+    path(
+        "", index, name="index"),
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
@@ -43,53 +44,37 @@ urlpatterns = [
     ),
     path("cars/",
          CarListView.as_view(),
-         name="car-list"
-         ),
+         name="car-list"),
     path("cars/<int:pk>/",
          CarDetailView.as_view(),
-         name="car-detail"
-         ),
+         name="car-detail"),
     path("cars/create/",
          CarCreateView.as_view(),
-         name="car-create"
-         ),
+         name="car-create"),
     path("cars/<int:pk>/update/",
          CarUpdateView.as_view(),
-         name="car-update"
-         ),
+         name="car-update"),
     path("cars/<int:pk>/delete/",
          CarDeleteView.as_view(),
-         name="car-delete"
-         ),
+         name="car-delete"),
     path("drivers/",
          DriverListView.as_view(),
-         name="driver-list"
-         ),
-    path(
-        "drivers/<int:pk>/",
-        DriverDetailView.as_view(),
-        name="driver-detail"
-    ),
-    path(
-        "cars/<int:pk>/assign/",
-        assign_driver_to_car,
-        name="assign"
-    ),
-    path(
-        "drivers/create/",
-        DriverCreateView.as_view(),
-        name="driver-create"
-    ),
-    path(
-        "drivers/<int:pk>/update/ ",
-        DriverUpdateView.as_view(),
-        name="driver-update"
-    ),
-    path(
-        "drivers/<int:pk>/delete/",
-        DriverDeleteView.as_view(),
-        name="driver-delete"
-    ),
+         name="driver-list"),
+    path("drivers/<int:pk>/",
+         DriverDetailView.as_view(),
+         name="driver-detail"),
+    path("cars/<int:pk>/assign/",
+         assign_driver_to_car,
+         name="assign"),
+    path("drivers/create/",
+         DriverCreateView.as_view(),
+         name="driver-create"),
+    path("drivers/<int:pk>/update/ ",
+         DriverUpdateView.as_view(),
+         name="driver-update"),
+    path("drivers/<int:pk>/delete/",
+         DriverDeleteView.as_view(),
+         name="driver-delete"),
 ]
 
 app_name = "taxi"
